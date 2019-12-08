a5979f899b8b5228a2ebff1fad58a3c823c60710
AdinAck <adinackerman@gmail.com>
2019-12-08 11:06:50 -0800

    ...

--- main.py
+++ main.py
@@ -71,6 +71,22 @@ class expand: # expand input into usable data
     def format(self,value):
         self.val = value
 
+def err1():
+    print("")
+    print("==Error==================")
+    print("Unable to expand input\ninto usable data,\nperhaps units were not\nspecified?")
+    print("==/Error/================")
+    print("")
+    menu()
+
+def err2():
+    print("")
+    print("==Error==================")
+    print("The number or position of\nunknowns in not\ncompatable with this\ncalculation.")
+    print("==/Error/================")
+    print("")
+    menu()
+
 def menu():
     print("==Menu===================")
     print("[1] Ohm's Law")
@@ -82,56 +98,96 @@ def menu():
     s = input("")
 
     if "1" in s:
-        print("")
-        print("==Ohm's Law==============")
-        print("V = IR")
-        print("Enter 0 if value unkown.")
-        print("")
-        v = expand()
-        i = expand()
-        r = expand()
-        try:
-            expand.incorporate(v, input("V = "))
-        except:
-            print("")
-            print("==Error==================")
-            print("Unable to expand input\ninto usable data,\nperhaps units were not\nspecified?")
-            print("==/Error/================")
-            print("")
-            menu()
-        expand.incorporate(i, input("I = "))
-        expand.incorporate(r, input("R = "))
-        a = np.array([v.val,i.val,r.val])
-        if len(np.where(a==0)[0]) >= 2:
-            print("Insufficient data.")
-        else:
-            v.val = v.val*v.scale
-            i.val = i.val*i.scale
-            r.val = r.val*r.scale
-            print("")
-            print("Solution:")
-            if v.val == 0:
-                v.val = float(i.val*r.val)
-                expand.unit(v,"V")
-                print("V = "+str(v.val)+" "+v.unit)
-            if i.val == 0:
-                i.val = float(v.val/r.val)
-                expand.unit(i,"A")
-                print("I = "+str(i.val)+" "+i.unit)
-            if r.val == 0:
-                r.val = float(v.val/i.val)
-                expand.unit(r,"Ohms")
-                print("R = "+str(r.val)+" "+r.unit)
-            w = expand()
-            expand.format(w, (i.val*i.scale)*(v.val*v.scale))
-            expand.unit(w, "W")
-            print("W = "+str(w.val)+" "+w.unit)
-        print("==/Ohm's Law/============")
-        print("")
+        ohmLaw()
     elif "2" in s:
-        pass
+        voltDiv()
     elif "3" in s:
         pass
 
     menu()
+
+def ohmLaw():
+    print("")
+    print("==Ohm's Law==============")
+    print("V = IR")
+    print("Enter 0 if value unkown.")
+    print("")
+    v = expand()
+    i = expand()
+    r = expand()
+    try:
+        expand.incorporate(v, input("V = "))
+    except TypeError:
+        err1()
+    try:
+        expand.incorporate(i, input("I = "))
+    except TypeError:
+        err1()
+    try:
+        expand.incorporate(r, input("R = "))
+    except TypeError:
+        err1()
+    a = np.array([v.val,i.val,r.val])
+    if len(np.where(a==0)[0]) != 1:
+        err2()
+    else:
+        v.val = v.val*v.scale
+        i.val = i.val*i.scale
+        r.val = r.val*r.scale
+        print("")
+        print("Solution:")
+        if v.val == 0:
+            v.val = float(i.val*r.val)
+            expand.unit(v,"V")
+            print("V = "+str(v.val)+" "+v.unit)
+        if i.val == 0:
+            i.val = float(v.val/r.val)
+            expand.unit(i,"A")
+            print("I = "+str(i.val)+" "+i.unit)
+        if r.val == 0:
+            r.val = float(v.val/i.val)
+            expand.unit(r,"Ohms")
+            print("R = "+str(r.val)+" "+r.unit)
+        w = expand()
+        expand.format(w, (i.val*i.scale)*(v.val*v.scale))
+        expand.unit(w, "W")
+        print("W = "+str(w.val)+" "+w.unit)
+    print("==/Ohm's Law/============")
+    print("")
+
+def voltDiv():
+    print("")
+    print("==Voltage Divider========")
+    print("Vout = Vin(R2/(R1 + R2))")
+    print("Enter 0 if value unkown.")
+    print("")
+    vin = expand()
+    vout = expand()
+    r1 = expand()
+    r2 = expand()
+    i = expand()
+    try:
+        expand.incorporate(vin, input("Vin = "))
+    except TypeError:
+        err1()
+    try:
+        expand.incorporate(vout, input("Vout = "))
+    except TypeError:
+        err1()
+    try:
+        expand.incorporate(r1, input("R1 = "))
+    except TypeError:
+        err1()
+    try:
+        expand.incorporate(r2, input("R2 = "))
+    except TypeError:
+        err1()
+    a = np.array([vin.val,vout.val,r1.val,r2.val])
+    if len(np.where(a==0)[0]) >= 3:
+        err2()
+    elif r1.val == 0 and r2.val == 0:
+        print("Additional information required:")
+        expand.incorporate(i, input("I = "))
+        v = vin.val-vout.val
+
 menu()
