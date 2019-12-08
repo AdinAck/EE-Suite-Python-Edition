8f02127a887e34e9c08ca652087bfa1e6f133cc2
AdinAck <adinackerman@gmail.com>
2019-12-07 17:47:37 -0800

    Ohm's law calculator completed

--- main.py
+++ main.py
@@ -6,17 +6,11 @@ print(" - By: Adin Ackerman")
 # time.sleep(1.5)
 for i in range(100):
     print("")
-print("==Menu===================")
-print("[1] Ohm's Law")
-print("[2] Voltage Divider")
-print("[3] Parallel Resistors")
-print("=========================")
-print("")
-
-s = input("")
 
 class expand: # expand input into usable data
-    def __init__(self,data):
+    def __init__(self):
+        pass
+    def incorporate(self,data):
         if int(data[0]) != 0:
             self.raw = data
             a = np.array([])
@@ -40,69 +34,104 @@ class expand: # expand input into usable data
                 self.scale = 10**(6)
             if "G" in self.unit[0]: # Giga
                 self.scale = 10**(9)
-
         else:
             self.val = 0
             self.scale = 0
 
-    def unit(self,v):
-        if self.val < 10**(-5):
-            self.val = self.val*10**(9)
-            self.unit = "n"+v
-        elif self.val < 10**(-2):
-            self.val = self.val*10**(6)
-            self.unit = "u"+v
+    def unit(self,variable):
+        if self.val < 10**(-6):
+            self.scale = 10**(-9)
+            self.val = round(self.val*10**(9),3)
+            self.unit = "n"+variable
+        elif self.val < 10**(-3):
+            self.scale = 10**(-6)
+            self.val = round(self.val*10**(6),3)
+            self.unit = "u"+variable
         elif self.val < 1:
-            self.val = self.val*10**(3)
-            self.unit = "m"+v
+            self.scale = 10**(-3)
+            self.val = round(self.val*10**(3),3)
+            self.unit = "m"+variable
         elif self.val < 10**(3):
-            self.val = self.val
-            self.unit = v
+            self.scale = 1
+            self.val = round(self.val,3)
+            self.unit = variable
         elif self.val < 10**(6):
-            self.val = self.val*10**(-3)
-            self.unit = "k"+v
+            self.scale = 10**(3)
+            self.val = round(self.val*10**(-3),3)
+            self.unit = "k"+variable
         elif self.val < 10**(9):
-            self.val = self.val*10**(-6)
-            self.unit = "M"+v
+            self.scale = 10**(6)
+            self.val = round(self.val*10**(-6),3)
+            self.unit = "M"+variable
         elif self.val < 10**(12) or self.val >=10**(12):
-            self.val = self.val*10**(-9)
-            self.unit = "G"+v
+            self.scale = 10**(9)
+            self.val = round(self.val*10**(-9),3)
+            self.unit = "G"+variable
 
-if "1" in s:
+    def format(self,value):
+        self.val = value
+
+def menu():
+    print("==Menu===================")
+    print("[1] Ohm's Law")
+    print("[2] Voltage Divider")
+    print("[3] Parallel Resistors")
+    print("==/Menu/=================")
     print("")
-    print("==Ohm's Law Calculator===")
-    print("V = IR")
-    print("Enter 0 if value unkown.")
-    print("")
-    v = expand(input("V = "))
-    i = expand(input("I = "))
-    r = expand(input("R = "))
-    a = np.array([v.val,i.val,r.val])
-    if len(np.where(a==0)[0]) >= 2:
-        print("Insufficient data.")
-    else:
-        v.val = v.val*v.scale
-        i.val = i.val*i.scale
-        r.val = r.val*r.scale
-        if v.val == 0:
-            v.val = float(i.val*r.val)
-        if i.val == 0:
-            i.val = float(v.val/r.val)
-        if r.val == 0:
-            r.val = float(v.val/i.val)
-        print("W: "+str(w))
-        expand.unit(v,"V")
-        expand.unit(i,"A")
-        expand.unit(r,"Ohms")
+
+    s = input("")
+
+    if "1" in s:
         print("")
-        print("Solution:")
-        print("V = "+str(v.val)+" "+v.unit)
-        print("I = "+str(i.val)+" "+i.unit)
-        print("R = "+str(r.val)+" "+r.unit)
-        # print("Power loss: "+str(w.val)+" "+w.unit)
-    print("==Ohm's Law Calculator===")
-    print("")
-elif "2" in s:
-    pass
-elif "3" in s:
-    pass
+        print("==Ohm's Law==============")
+        print("V = IR")
+        print("Enter 0 if value unkown.")
+        print("")
+        v = expand()
+        i = expand()
+        r = expand()
+        try:
+            expand.incorporate(v, input("V = "))
+        except:
+            print("")
+            print("==Error==================")
+            print("Unable to expand input\ninto usable data,\nperhaps units were not\nspecified?")
+            print("==/Error/================")
+            print("")
+            menu()
+        expand.incorporate(i, input("I = "))
+        expand.incorporate(r, input("R = "))
+        a = np.array([v.val,i.val,r.val])
+        if len(np.where(a==0)[0]) >= 2:
+            print("Insufficient data.")
+        else:
+            v.val = v.val*v.scale
+            i.val = i.val*i.scale
+            r.val = r.val*r.scale
+            print("")
+            print("Solution:")
+            if v.val == 0:
+                v.val = float(i.val*r.val)
+                expand.unit(v,"V")
+                print("V = "+str(v.val)+" "+v.unit)
+            if i.val == 0:
+                i.val = float(v.val/r.val)
+                expand.unit(i,"A")
+                print("I = "+str(i.val)+" "+i.unit)
+            if r.val == 0:
+                r.val = float(v.val/i.val)
+                expand.unit(r,"Ohms")
+                print("R = "+str(r.val)+" "+r.unit)
+            w = expand()
+            expand.format(w, (i.val*i.scale)*(v.val*v.scale))
+            expand.unit(w, "W")
+            print("W = "+str(w.val)+" "+w.unit)
+        print("==/Ohm's Law/============")
+        print("")
+    elif "2" in s:
+        pass
+    elif "3" in s:
+        pass
+
+    menu()
+menu()
